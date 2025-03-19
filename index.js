968da64df4415ad36246f058e24f297c51fe44c4
Alenia24 <112526474+Alenia24@users.noreply.github.com>
2025-03-19 00:07:09 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,120 @@
+let taskForm = document.getElementById("taskForm");
+let taskName = document.querySelector("#taskName");
+let taskDate = document.querySelector("#taskDate");
+let taskCount = document.querySelector("#count");
+let tasksCreated = document.querySelector(".tasksCreated");
+
+let errorDisplay = document.querySelector(".errorDisplay");
+
+let taskCounter = 0;
+
+function showError() {
+  errorDisplay.style.display = "block";
+}
+
+function validateTask(event) {
+  event.preventDefault();
+  errorDisplay.style.display = "none";
+
+  const taskNameVal = validateTaskName();
+  if (taskNameVal === false) {
+    showError();
+    return false;
+  }
+
+  const taskDateVal = validateDate();
+  if (taskDateVal === false) {
+    showError();
+    return false;
+  }
+
+  if (true) {
+    storeValues();
+    displayTask();
+  }
+
+  return true;
+}
+
+function validateTaskName() {
+  let name = taskName.value;
+
+  if (name === "") {
+    errorDisplay.textContent = "Task Name cannot be empty";
+    return false;
+  }
+}
+
+function validateDate() {
+  let date = new Date(taskDate.value);
+  let currentDate = new Date();
+  if (!date) {
+    errorDisplay.textContent = "You must enter a date";
+    return false;
+  }
+
+  if (date <= currentDate) {
+    errorDisplay.textContent = "Date cannot be in the past";
+    return false;
+  }
+}
+
+function storeValues() {
+  let nameStorage = taskName.value;
+  let dateStorage = taskDate.value;
+
+  const oldTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+  let task = {};
+
+  Object.assign(task, {
+    newTaskName: `${nameStorage}`,
+    newTaskDate: `${dateStorage}`,
+  });
+
+  oldTasks.push(task);
+  localStorage.setItem("tasks", JSON.stringify(oldTasks));
+
+  // Clear all form fields
+  taskForm.reset();
+}
+
+function displayTask() {
+  const oldTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+  let newEl = (s) => document.createElement(s);
+
+  oldTasks.forEach((task) => {
+    let taskDiv = tasksCreated.appendChild(newEl("div"));
+    taskDiv.classList.add("taskElement");
+    let taskName = taskDiv.appendChild(newEl("p"));
+    taskName.classList.add("taskss");
+    taskName.innerHTML = task.newTaskName;
+    let taskDate = taskDiv.appendChild(newEl("p"));
+    taskDate.classList.add("taskss");
+    taskDate.innerHTML = task.newTaskDate;
+    let taskComplete = taskDiv.appendChild(newEl("button"));
+    taskComplete.appendChild(document.createTextNode(`Mark as Done`));
+    taskComplete.classList.add("taskComplete");
+  });
+  taskCounter++;
+  taskCount.textContent = `${taskCounter}`;
+  alert("Task Added");
+}
+
+function deleteTask(event) {
+  event.preventDefault();
+  if (event.target.classList.contains("taskComplete")) {
+    let task = event.target.parentElement;
+    let btn = event.target;
+    task.style.textDecoration = "line-through";
+    setTimeout(alert("Task Completed 🥳", 5000));
+    btn.style.backgroundColor = "green";
+    event.target.textContent = "Completed";
+    taskCounter--;
+    taskCount.textContent = `${taskCounter}`;
+  }
+}
+taskForm.addEventListener("submit", validateTask);
+
+tasksCreated.addEventListener("click", deleteTask);
